46d9368f119cfb46d792b04d3eee59ec7af13960
mootaz akermi <datasolution1001@gmail.com>
2023-09-14 22:46:48 +0100

    still working on the project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 
-let arrowBox = document.querySelectorAll(".scroll-btn");
+let arrowBox = document.querySelector(".scroll-btn");
 let arrowAnimation = document.querySelector(".arrow-animation");
 let burgerBar = document.querySelector(".burger-bar");
 let navLinks = document.querySelector(".links");
 let exitBtn = document.querySelector(".exit-btn");
+let firstPage = document.querySelector(".first-page");
+let secondOne = document.querySelector(".first-page .secondOne");
+let secondTwo = document.querySelector(".first-page .secondTwo");
+let third = document.querySelector(".first-page .third");
+let fourth = document.querySelector(".first-page .fourth");
+let sixthVid = document.querySelector(".first-page .sixth .vid");
+
 
 let tl2 = gsap.timeline();
 
@@ -17,7 +24,7 @@ burgerBar.onclick = () => {
 exitBtn.onclick = () => {
     burgerBar.style.display= "block";
     exitBtn.style.display = "none";
-    tl2.to(".links", {display: "none", "--clip": '0%',autoAlpha:1, duration: 0.75});
+    tl2.to(".links", {display: "none", "--clip": '0%' ,autoAlpha:1, duration: 0.75});
     
 }
 
@@ -42,31 +49,18 @@ function arrowAnimationTwo() {
     tl.pause();
     gsap.to(".arrow-animation", {y: 0, duration: 0.5})
 }
-arrowBox[0].onmouseenter = () => {
+arrowBox.onmouseenter = () => {
    
         arrowAnimationOne();
     
 }
-arrowBox[1].onmouseenter = () => {
-    arrowAnimationOne();
-}
-arrowBox[2].onmouseenter = () => {
-    arrowAnimationOne();
 
+
+arrowBox.onmouseleave = () => {
+    
+    arrowAnimationTwo();
 }
 
-arrowBox[0].onmouseleave = () => {
-    
-    arrowAnimationTwo();
-}
-arrowBox[1].onmouseleave = () => {
-    
-    arrowAnimationTwo();
-}
-arrowBox[2].onmouseleave = () => {
-    
-    arrowAnimationTwo();
-}
 
 // Swiper js
 const swiper = new Swiper('.swiper', {
@@ -80,3 +74,50 @@ const swiper = new Swiper('.swiper', {
   
 
   });
+
+// Start  Animation First section
+let tl3 = gsap.timeline();
+let scrollCounter = 0;
+function animaitonFirstPage ()  {
+    switch (scrollCounter) {
+        case 0: firstPage.style.cssText = "z-index: 5";
+                tl3
+                .add('start')
+                .to(".first-page .first", {left: 0, duration: 0.75,delay: 0.25, ease: "power3"},'start')
+                .to(".first-page .secondOne", {right: 0, duration: 0.75,delay: 0.25,  ease: "power3"} , 'start')
+                .to(".first-page .secondTwo", {right: 0, duration: 0.75,delay: 0.25,  ease: "power3"} , 'start')
+                .to(".first-page .third", {right: 0});
+                scrollCounter += 1;
+                break;
+        case 1: 
+                tl3
+                .add('second')
+                .to(".first-page .first", {left: "-50%", duration: 0.75, delay: 0.25, ease: "power3" }, 'second')
+                .to(".first-page .third", {left: 0, duration: 0.75,delay: 0.25, ease: "power3"}, 'second')
+                .to(".first-page .secondOne .second ", {left: "-100%",duration: 0.75, delay: 0.25, ease: "power3"},'second')
+                .to(".first-page .fourth", {right: 0, zIndex: 5 , duration: 0.75, delay: 0.25, ease: "power3"},'second');
+                scrollCounter += 1;
+                break;
+        case 2: 
+                tl3
+                .add('third')
+                .to(".first-page .secondTwo", {left: "0%",duration: 0.75, delay: 0.25, ease: "power3"},'third')
+                .to(".first-page .fourth", {left: 0, zIndex: 5 , duration: 0.75, delay: 0.25, ease: "power3"},'third')
+                .to(".first-page .fifth", {left: 0, duration: 0.5, delay: 0.25, ease: "power3" }, 'third')
+                .to(".first-page .sixth", {left: 0, duration: 0.5,delay: 0.25, ease: "power3"}, 'third')
+                .to(".first-page .seventh", {right: 0,duration: 0.5, delay: 0.25, ease: "power3"},'third');
+                sixthVid.play();
+                scrollCounter += 1;
+                break;
+        
+    }
+    
+    
+}
+arrowBox.onclick = () => {
+   
+    animaitonFirstPage();
+
+}
+
+// Edn Animation First section
